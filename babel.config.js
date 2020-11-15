ff5cbbb814b81188c357d610129d988da391fbfd
Rajab <rajabm259@gmail.com>
2020-11-15 09:43:11 -0800

    player vue

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,4 @@
 module.exports = {
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
-    }),
-    new MyAwesomeWebpackPlugin()
-  ],
   presets: [
     '@vue/cli-plugin-babel/preset'
   ]
